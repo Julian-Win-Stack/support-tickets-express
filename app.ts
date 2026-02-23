ba0a40893b3f260a2fd277246ca8e2817ecef4dd
Julian-Win-Stack <9vfxzg7vhz@privaterelay.appleid.com>
2026-02-23 10:48:35 -0800

    fix: set session cookie sameSite to none and secure to true

--- app.ts
+++ app.ts
@@ -47,8 +47,8 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      sameSite: "lax",
-      secure: false,
+      sameSite: "none",
+      secure: true,
       maxAge: 1000 * 60 * 60 * 24 // 1 day
     }
   })
