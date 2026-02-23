e95eed2f7558e0104f81080e10544a1f37b7f7c7
Julian-Win-Stack <9vfxzg7vhz@privaterelay.appleid.com>
2026-02-23 11:41:32 -0800

    refactor: use CORS_ORIGIN env variable for CORS origin

--- app.ts
+++ app.ts
@@ -29,7 +29,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
     credentials: true,
   })
 );
